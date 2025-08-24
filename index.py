490d9a3920a87ee7c6c9d797a2183e458730892b
misha5252 <beskarevmihail0@gmail.com>
2025-08-24 15:03:56 +0300

    new commit

--- index.py
+++ index.py
@@ -1,1 +1,8 @@
-print("миша очень классный")
+print("Введите пароль:")
+true_password = 654321
+a = int(input())
+
+if a == true_password:
+    print("Вход в систему")
+else:
+    print("Пароль не верный")
